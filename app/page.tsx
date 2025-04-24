1bc976e4fac40404a906092345808a809ad689ae
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-04-24 14:27:37 +0300

    refactor(home): reorganize components into home directory for better structure
    Moved all home-related components into a dedicated `home` directory to improve project organization and maintainability. This change makes it easier to manage and locate components specific to the home page, reducing clutter in the main components folder.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import CTA from "@/components/CTASection";
-import FAQ from "@/components/FAQSection";
-import Features from "@/components/Features";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import HowItWorks from "@/components/HowItWorks";
-import Integrations from "@/components/Integrations";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonial";
-import WhyChooseUs from "@/components/WhyChooseUs";
+import CTA from "@/components/home/CTASection";
+import FAQ from "@/components/home/FAQSection";
+import Features from "@/components/home/Features";
+import Footer from "@/components/home/Footer";
+import Header from "@/components/home/Header";
+import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
+import Integrations from "@/components/home/Integrations";
+import Pricing from "@/components/home/Pricing";
+import Testimonials from "@/components/home/Testimonial";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
 
 export default function Home() {
   return (
