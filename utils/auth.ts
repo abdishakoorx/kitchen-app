b958939e9e17cf455eb4251785cb19767cceacc7
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-06-23 17:34:54 +0300

    feat(auth): add auth client
    - Replace pg Pool with drizzle adapter for database operations
    - Add new auth client for Next.js frontend usage
    - Define database schema for auth-related tables
    - Enable email/password authentication

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
-import { Pool } from "pg";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
+import { schema } from "@/db/Schema";
+
 
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
   plugins: [nextCookies()],
 });
